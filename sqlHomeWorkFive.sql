dcfb11f441d6f42b43adab65c7ba5f58ba7b6355
RONSVER <126710676+RONSVER@users.noreply.github.com>
2024-07-22 15:42:01 +0200

    sql home work five
    practic with "join" functions

---
+++ sqlHomeWorkFive.sql
@@ -0,0 +1,93 @@
+-- creating and filling a database
+
+CREATE DATABASE shop;
+
+USE shop;
+
+CREATE TABLE SELLERS(
+       SELL_ID    INTEGER, 
+       SNAME   VARCHAR(20), 
+       CITY    VARCHAR(20), 
+       COMM    NUMERIC(2, 2),
+             BOSS_ID  INTEGER
+);
+                                            
+CREATE TABLE CUSTOMERS(
+       CUST_ID    INTEGER, 
+       CNAME   VARCHAR(20), 
+       CITY    VARCHAR(20), 
+       RATING  INTEGER
+);
+
+CREATE TABLE ORDERS(
+       ORDER_ID  INTEGER, 
+       AMT     NUMERIC(7,2), 
+       ODATE   DATE, 
+       CUST_ID    INTEGER,
+       SELL_ID    INTEGER 
+);
+
+INSERT INTO SELLERS VALUES(201,'Олег','Москва',0.12,202);
+INSERT INTO SELLERS VALUES(202,'Лев','Сочи',0.13,204);
+INSERT INTO SELLERS VALUES(203,'Арсений','Владимир',0.10,204);
+INSERT INTO SELLERS VALUES(204,'Екатерина','Москва',0.11,205);
+INSERT INTO SELLERS VALUES(205,'Леонид ','Казань',0.15,NULL);
+
+
+INSERT INTO CUSTOMERS VALUES(301,'Андрей','Москва',100);
+INSERT INTO CUSTOMERS VALUES(302,'Михаил','Тула',200);
+INSERT INTO CUSTOMERS VALUES(303,'Иван','Сочи',200);
+INSERT INTO CUSTOMERS VALUES(304,'Дмитрий','Ярославль',300);
+INSERT INTO CUSTOMERS VALUES(305,'Руслан','Москва',100);
+INSERT INTO CUSTOMERS VALUES(306,'Артём','Тула',100);
+INSERT INTO CUSTOMERS VALUES(307,'Юлия','Сочи',300);
+
+
+INSERT INTO ORDERS VALUES(101,18.69,'2022-03-10',308,207);
+INSERT INTO ORDERS VALUES(102,5900.1,'2022-03-10',307,204);
+INSERT INTO ORDERS VALUES(103,767.19,'2022-03-10',301,201);
+INSERT INTO ORDERS VALUES(104,5160.45,'2022-03-10',303,202);
+INSERT INTO ORDERS VALUES(105,1098.16,'2022-03-10',308,207);
+INSERT INTO ORDERS VALUES(106,75.75,'2022-04-10',304,202); 
+INSERT INTO ORDERS VALUES(107,4723,'2022-05-10',306,201);
+INSERT INTO ORDERS VALUES(108,1713.23,'2022-04-10',302,203);
+INSERT INTO ORDERS VALUES(109,1309.95,'2022-06-10',304,203);
+INSERT INTO ORDERS VALUES(110,9891.88,'2022-06-10',306,201);
+
+-- ---------------------------------------------------------------------------
+
+
+-- Выведите имена всех продавцов. Предусмотрите также в выборке имена их боссов, сформировав атрибут bossname. 
+-- В выборке должно присутствовать два атрибута — sname, bossname.
+
+select t1.sname as seller_name, t2.sname as boss_name
+from sellers t1
+inner join sellers t2
+on t1.boss_id = t2.sell_id;
+
+-- Выведите имена покупателей, которые сделали заказ.
+-- Предусмотрите в выборке также имена продавцов.
+-- Примечание: покупатели и продавцы должны быть из разных городов.
+-- В выборке должно присутствовать два атрибута — cname, sname.
+
+select 
+cust.cname as customer_name,
+cust.city as customer_city,
+sell.sname as seller_name,
+sell.city as seller_city
+from customers cust
+inner join orders ord on cust.cust_id = ord.cust_id
+inner join sellers sell on ord.sell_id = sell.sell_id
+where cust.city <> sell.city;
+ 
+-- Выведите пары покупателей и обслуживших их продавцов из одного города.
+-- Вывести: sname, cname, city
+
+select 
+sell.sname as sname,
+cust.cname as cname,
+cust.city as city
+from customers cust
+inner join orders ord on cust.cust_id = ord.cust_id
+inner join sellers sell on ord.sell_id = sell.sell_id
+where cust.city = sell.city;
